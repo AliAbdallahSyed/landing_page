bdf5ca0cee0a74c1e6c7d5ae4744509885f60150
unknown <aliabdallahsyed@gmail.com>
2021-10-21 20:06:45 +0200

    adding scroll to top button

--- js/app.js
+++ js/app.js
@@ -1,62 +1,44 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
+var AllSections = Array.from(document.querySelectorAll('section'));
+var NumberOfSection = AllSections.length;
+var ListItem = document.getElementById("navbar__list");
+const toTop = document.querySelector(".to-top");
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+function ListCreator() {
+    for (section of AllSections){
+        SectionName = section.getAttribute("data-nav");
+        sectionLink = section.getAttribute("id");
+        listItem = document.createElement("li");
+        listItem.innerHTML = `<a class='menu__link' href='#${sectionLink}'>${SectionName}</a>`
+        ListItem.appendChild(listItem);
+    }
+}
+
+function sectionView(element){
+    let sectionPos = element.getBoundingclientRect();
+    return (sectionPos.top >=0 );
+}
+
+function toggleActiveClass(){
+    for (section of sections) {
+        if (sectionView(section)) {
+            if (!section.classlist.contains("your-active-class")){
+                section.classlist.add("your-active-class")
+            }
+        } else {
+            section.classlist.remove("you-active-class")
+        }
+    }   
+}
 
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+      toTop.classList.add("active");
+    } else {
+      toTop.classList.remove("active");
+    }
+  })
+  
+ListCreator();
+document.addEventListener('scroll', toggleActiveClass)
