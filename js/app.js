ab1492be9852f256ba860f2f0b62b9ee7adbbaaf
unknown <aliabdallahsyed@gmail.com>
2021-10-21 20:32:23 +0200

    add comments

--- js/app.js
+++ js/app.js
@@ -1,44 +1,25 @@
-var AllSections = Array.from(document.querySelectorAll('section'));
-var NumberOfSection = AllSections.length;
-var ListItem = document.getElementById("navbar__list");
-const toTop = document.querySelector(".to-top");
+var AllSections = Array.from(document.querySelectorAll('section')); // put all section in array to count them
+var ListItem = document.getElementById("navbar__list"); // take the (ul) element to control it
+const toTop = document.querySelector(".to-top"); //
 
-
+// this function is used to create a list that is going to appear in the main page
 function ListCreator() {
     for (section of AllSections){
-        SectionName = section.getAttribute("data-nav");
-        sectionLink = section.getAttribute("id");
-        listItem = document.createElement("li");
-        listItem.innerHTML = `<a class='menu__link' href='#${sectionLink}'>${SectionName}</a>`
-        ListItem.appendChild(listItem);
+        Name = section.getAttribute("data-nav"); // take the name of the section
+        Link = section.getAttribute("id"); //  the the id of the section
+        Item = document.createElement("li"); // create a new list in the (ul)
+        Item.innerHTML = `<a class='menu__link' href='#${Link}'>${Name}</a>` // create the link of the section
+        ListItem.appendChild(Item); // add the child (li) to (ul)
     }
 }
 
-function sectionView(element){
-    let sectionPos = element.getBoundingclientRect();
-    return (sectionPos.top >=0 );
-}
-
-function toggleActiveClass(){
-    for (section of sections) {
-        if (sectionView(section)) {
-            if (!section.classlist.contains("your-active-class")){
-                section.classlist.add("your-active-class")
-            }
-        } else {
-            section.classlist.remove("you-active-class")
-        }
-    }   
-}
-
-
+// this function is used in scroll to top button
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 100) {
       toTop.classList.add("active");
     } else {
       toTop.classList.remove("active");
     }
-  })
-  
+})
+
 ListCreator();
-document.addEventListener('scroll', toggleActiveClass)
